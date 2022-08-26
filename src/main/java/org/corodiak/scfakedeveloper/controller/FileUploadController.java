cf3e6863fc08d596c6ff020d7d9c21c23476b7bc
ncw809 <ncw809@naver.com>
2022-08-26 11:34:27 +0900

    feat: 텔레그램 관리자 알림 기능 추가

--- src/main/java/org/corodiak/scfakedeveloper/controller/FileUploadController.java
+++ src/main/java/org/corodiak/scfakedeveloper/controller/FileUploadController.java
@@ -6,6 +6,7 @@ import org.corodiak.scfakedeveloper.exception.FileUploadFailException;
 import org.corodiak.scfakedeveloper.service.EmailService;
 import org.corodiak.scfakedeveloper.service.FileUploadService;
 import org.corodiak.scfakedeveloper.type.dto.ResponseModel;
+import org.corodiak.scfakedeveloper.util.TelegramMessageBot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,9 @@ import org.springframework.web.multipart.MultipartFile;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,14 +29,22 @@ public class FileUploadController {
 
 	private final FileUploadService fileUploadService;
 	private final EmailService emailService;
+	private final TelegramMessageBot telegramMessageBot;
 
 	@Operation(summary = "파일 업로드", description = "ROLE_ADMIN")
 	@Secured({"ROLE_ADMIN"})
 	@RequestMapping(value = "/upload", method = RequestMethod.POST)
 	public ResponseModel upload(
 		@RequestParam(value = "file", required = true) MultipartFile multipartFile
-	) throws FileUploadFailException {
+	) throws FileUploadFailException, TelegramApiException {
 		String url = fileUploadService.saveFile(multipartFile);
+		LocalDateTime dateTime = LocalDateTime.now();
+		String message = "\n===============ALERT===============\n"
+				+ "============FILE UPLOAD============\n"
+				+ "[File URL] : " + url
+				+ "[Upoad Time] : " + dateTime;
+
+		telegramMessageBot.sendMessage(message);
 		ResponseModel responseModel = ResponseModel.builder().build();
 		responseModel.addData("url", url);
 		return responseModel;
