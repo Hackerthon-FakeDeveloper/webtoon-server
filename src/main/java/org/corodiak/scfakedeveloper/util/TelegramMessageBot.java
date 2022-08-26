f3540caed2b8d04e95b0d8dc6532bf9dd6f0cc70
ncw809 <ncw809@naver.com>
2022-08-26 10:50:56 +0900

    feat: 텔레그램 유틸 추가

---
+++ src/main/java/org/corodiak/scfakedeveloper/util/TelegramMessageBot.java
@@ -0,0 +1,43 @@
+package org.corodiak.scfakedeveloper.util;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class TelegramMessageBot extends TelegramLongPollingBot {
+    @Value("${bot.BOT_TOKEN}")
+    private String botToken;
+
+    @Value("${bot.BOT_USERNAME}")
+    private String botUsername;
+
+    @Value("${bot.CHAT_ID}")
+    private String chatId;
+
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    public void sendMessage(String message) throws TelegramApiException {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(message);
+
+        execute(sendMessage);
+    }
+}
